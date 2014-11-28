7d062d217407ed45019750ddbb6547a4c76966a9
zzy <zzy@holidaycloud.cn>
2014-11-28 10:01:34 +0800

    111

--- control/payLogCtrl.js
+++ control/payLogCtrl.js
@@ -1,6 +1,8 @@
 /**
  * Created by zzy on 2014/11/28.
  */
+var request = require('request');
+var config = require('./../config/config.json');
 var PayLogCtrl = function(){};
 PayLogCtrl.save = function(params,fn){
     var url = config.inf.host+":"+config.inf.port+"/api/payLog/save";
