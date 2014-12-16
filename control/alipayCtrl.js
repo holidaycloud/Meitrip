d1b61c8e45d003355d5f1a36115b499137a15bdb
zzy <zzy@holidaycloud.cn>
2014-12-16 09:02:33 +0800

    wapNotifySign

--- control/alipayCtrl.js
+++ control/alipayCtrl.js
@@ -151,9 +151,9 @@ AlipayCtrl.wapNotify = function (pid,key,params,fn){
                     }
                 }
             });
-
         }]
     },function(err,results){
+        console.log(err,results);
         if(err){
             fn(err,null);
         } else {
